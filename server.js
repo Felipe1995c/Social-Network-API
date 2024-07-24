5e77fea0dcb947cce154be16e1e21aca567af2ee
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-23 19:10:51 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
