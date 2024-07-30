a1965ed96eb97be534e0180dc89e2912427b6d9c
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-29 18:30:39 -0700

    Added some comments and packages needed

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,27 +1,32 @@
 const router = require('express').Router();
+
+
 const {
-    getThoughts,
+    getThought,
     getSingleThought,
     createThought,
     updateThought,
     deleteThought,
-    addReaction,
-    removeReaction
+    createReaction,
+    deleteReaction
 } = require('../../controllers/thoughtController');
 
-router.route('/')
-    .get(getThoughts)
-    .post(createThought);
+// /api/thoughts GET all and POST thought
+router.route('/').get(getThought).post(createThought);
 
+// /api/thoughts/:thoughtId GET one thought, PUT and DELETE by iD
 router.route('/:thoughtId')
-    .get(getSingleThought)
-    .put(updateThought)
-    .delete(deleteThought);
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
 
+//  /api/thoughts/:thoughtId/reactions POST new reactions
 router.route('/:thoughtId/reactions')
-    .post(addReaction);
+.post(createReaction);
 
+// /api/thoughts/:thoughtId/reactions/:reactionId DELETE reaction by ID
 router.route('/:thoughtId/reactions/:reactionId')
-    .delete(removeReaction);
+.delete(deleteReaction);
+
 
 module.exports = router;
