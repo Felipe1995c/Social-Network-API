26099ebd16fbdad61ce816160380d21c76a59fa4
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-30 21:59:04 -0700

    still working on thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -19,5 +19,36 @@ module.exports = {
         )
         .catch((error) => res.status(500).json(error));
     },
-    
+    // Create a thought and push the created thought's _id to the correct user's thought
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then((thought) => 
+        !thought
+        ? res.status(404).json({ message: "No User find with this ID!" })
+        : res.json(thought)
+    ).catch((error) => res.status(500).json(error));
+    },
+    // Update thoughts
+updateThought(req, res) {
+    Thought.FindOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, New: true }
+    )
+    .then((user) => 
+    !user
+    ? res.status(404).json({ message: "No thought find with this ID!"})
+    : res.json(user)
+)
+.catch((error) => res.json(500).json(error));
+},
+
+// Delete thought
 }
