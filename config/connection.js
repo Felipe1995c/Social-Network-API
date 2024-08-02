5bb399250eda674b03a6e6c90a13096bcdbdbeb5
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-08-01 19:13:12 -0700

    Fixed/ connected mongodb. Still getting an error refusing localhost to connect but its a new error

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetwork';
 
 connect(connectionString, {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNetwork', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
